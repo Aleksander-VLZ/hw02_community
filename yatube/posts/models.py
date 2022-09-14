a59b032a81be4d359a80e2ecc157ba5580b7f658
Александр Ратушнюк <112060297+Aleksander-VLZ@users.noreply.github.com>
2022-09-14 12:45:06 +0300

    14 new

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+# Из модуля auth импортируем функцию get_user_model
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Group(models.Model):  # наследник класса Model из модуля models
+    title = models.TextField(max_length=200)  # название сообщества
+    slug = models.SlugField(unique=True)  # адрес сообщества
+    description = models.TextField()  # описание сообщества
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Post(models.Model):  # наследник класса Model из модуля models
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,13 +1,27 @@
 # yatube/views.py
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post, Group
 
 
-# Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 # Для страницы, на которой будут посты, отфильтрованные по группам;
-# view-функция принимает параметр pk из path()
-def group_posts(request, pk):
-    return HttpResponse(f'Пост {pk}')
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
