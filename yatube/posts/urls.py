2f4f5b7b84c1f6f48b5b55329a50ecfd1a66955a
Александр Ратушнюк <112060297+Aleksander-VLZ@users.noreply.github.com>
2022-09-16 01:01:21 +0300

    16 css

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'posts/'
+app_name = 'posts'
 
 urlpatterns = [
+    #  Главная страница
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts, name='group_list')
+    #  Страница сообществ
+    path('group/<slug:slug>/', views.group_posts, name='group_posts')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -6,22 +6,24 @@ from .models import Post, Group
 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
+    template = 'posts/index.html'
     context = {
         'posts': posts,
     }
-    return render(request, 'posts/index.html', context)
+    return render(request, template, context)
 
 
 # Для страницы, на которой будут посты, отфильтрованные по группам;
 def group_posts(request, slug):
-    # Функция get_object_or_404 получает по заданным критериям объект 
+    # Функция get_object_or_404 получает по заданным критериям объект,
     # из базы данных или возвращает сообщение об ошибке, если объект не найден.
     # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
+    # поле slug у которых соответствует значению slug в запросе.
     group = get_object_or_404(Group, slug=slug)
     posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    template = 'posts/group_list.html'
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, 'posts/group_list.html', context)
+    return render(request, template, context)
